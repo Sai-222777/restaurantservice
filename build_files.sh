cb6a87d0ff3162adbe255ef732dfbd654e08a510
Sai-222777 <Saishree.Vardhan@iiitb.ac.in>
2024-10-06 14:35:16 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-# Activate Vercel's default Python environment
-python3.9 -m venv venv
-source venv/bin/activate
-
-# Install requirements
-venv/bin/pip install -r requirements.txt
-
-# Collect static files
-venv/bin/python3.9 manage.py collectstatic --noinput
+pip install -r requirements.txt 
+python3.9 manage.py collectstatic
